31804c6ebf1f36be61dd14e142b4b599efe8e759
gaocanjun <2609087666@qq.com>
2024-11-11 21:48:44 +0800

    update files

---
+++ Unity19_Sample/Assets/AStar/Pipe.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe
+{
+    public Vector2Int Location { get; private set; }
+    public Grid ParentGrid { get; private set; }
+    public Sprite Graphic { get; private set; }
+    public List<Direction> ConnectionDirections { get; private set; }
+    public bool Filled { get; private set; }
+    public bool Leaked { get; private set; }
+    public bool Pump { get; private set; }
+    public string Type { get; private set; }
+    public bool Replaceable { get; private set; }
+    public bool CanLeak { get; private set; }
+
+    public Pipe(Sprite graphic, List<Direction> connectionDirections, string type, bool replaceable, bool canLeak = true)
+    {
+        Location = Vector2Int.zero;
+        ParentGrid = null;
+        Graphic = graphic;
+        ConnectionDirections = connectionDirections;
+        Filled = false;
+        Leaked = false;
+        Pump = false;
+        Type = type;
+        Replaceable = replaceable;
+        CanLeak = canLeak;
+    }
+
+    public void Attach(Grid grid, Vector2Int location)
+    {
+        ParentGrid = grid;
+        Location = location;
+    }
+
+    public void Detach()
+    {
+        if (ParentGrid == null)
+            return;
+
+        var parent = ParentGrid;
+        ParentGrid = null;
+        parent.RemovePipe(this);
+        Location = Vector2Int.zero;
+    }
+
+    public void Fill(Direction dir, List<Pipe> filledPipes = null)
+    {
+        if (filledPipes == null)
+        {
+            filledPipes = new List<Pipe>();
+        }
+
+        if (filledPipes.Contains(this))
+        {
+            return;
+        }
+
+        filledPipes.Add(this);
+
+        if (Filled && Type != "Drain")
+        {
+            var pipeArray = GetConnections(dir);
+
+            if (CanLeak && GetConnections(null).Count != ConnectionDirections.Count)
+            {
+                Leaked = true;
+            }
+
+            foreach (var pipe in pipeArray)
+            {
+                var delta = Location - pipe.Location;
+                pipe.Fill(Direction.FromVector(delta), filledPipes);
+            }
+        }
+        else
+        {
+            Filled = true;
+        }
+    }
+
+    public void Draw(SpriteRenderer renderer, float x, float y)
+    {
+        renderer.sprite = Graphic;
+        renderer.transform.position = new Vector3(x, y, 0);
+    }
+
+    public List<Pipe> GetConnections(Direction dir)
+    {
+        var pipeArray = new List<Pipe>();
+        var pipeUp = ParentGrid.GetPipe(Location + Direction.Up.Delta);
+        var pipeDown = ParentGrid.GetPipe(Location + Direction.Down.Delta);
+        var pipeRight = ParentGrid.GetPipe(Location + Direction.Right.Delta);
+        var pipeLeft = ParentGrid.GetPipe(Location + Direction.Left.Delta);
+
+        if (dir== Direction.Up)
+        {
+            pipeUp = null;
+        }
+        else if (dir == Direction.Down)
+        {
+            pipeDown = null;
+        }
+        else if (dir == Direction.Right)
+        {
+            pipeRight = null;
+        }
+        else if (dir == Direction.Left)
+        {
+            pipeLeft = null;
+        }
+
+        foreach (var direction in ConnectionDirections)
+        {
+            if (dir == Direction.Up)
+            {
+                if (pipeUp != null && pipeUp.ConnectionDirections.Contains(Direction.Down))
+                {
+                    pipeArray.Add(pipeUp);
+                }
+            }
+            else if (dir == Direction.Down)
+            {
+                if (pipeDown != null && pipeDown.ConnectionDirections.Contains(Direction.Up))
+                {
+                    pipeArray.Add(pipeDown);
+                }
+            }
+            else if (dir == Direction.Right)
+            {
+                if (pipeRight != null && pipeRight.ConnectionDirections.Contains(Direction.Left))
+                {
+                    pipeArray.Add(pipeRight);
+                }
+            }
+            else if (dir == Direction.Left)
+            {
+                if (pipeLeft != null && pipeLeft.ConnectionDirections.Contains(Direction.Right))
+                {
+                    pipeArray.Add(pipeLeft);
+                }
+            }
+        }
+
+        return pipeArray;
+    }
+
+    public void Update(float deltaTime)
+    {
+        // Update logic if needed
+    }
+
+    public bool ConnectedToPump()
+    {
+        var pipeArray = GetConnections(null);
+        foreach (var pipe in pipeArray)
+        {
+            if (pipe.Pump)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void SetAsPump()
+    {
+        Pump = true;
+    }
+
+    public bool IsPump()
+    {
+        return Pump;
+    }
+
+    public bool IsFilled()
+    {
+        return Filled;
+    }
+    public bool IsLeaking()
+    {
+        return Leaked;
+    }
+
+    public bool CanReplace()
+    {
+        return !Filled && Replaceable;
+    }
+}
---
+++ Unity19_Sample/Assets/AStar/Pipes.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes
+{
+    public static readonly PipeType Vertical = new PipeType(0, "gfx/vertical.png", new List<Direction> { Direction.Up, Direction.Down }, true);
+    public static readonly PipeType Horizontal = new PipeType(1, "gfx/horizontal.png", new List<Direction> { Direction.Left, Direction.Right }, true);
+    public static readonly PipeType RightDown = new PipeType(2, "gfx/rightDown.png", new List<Direction> { Direction.Right, Direction.Down }, true);
+    public static readonly PipeType LeftDown = new PipeType(3, "gfx/leftDown.png", new List<Direction> { Direction.Left, Direction.Down }, true);
+    public static readonly PipeType RightUp = new PipeType(4, "gfx/rightUp.png", new List<Direction> { Direction.Right, Direction.Up }, true);
+    public static readonly PipeType LeftUp = new PipeType(5, "gfx/leftUp.png", new List<Direction> { Direction.Left, Direction.Up }, true);
+
+    public static readonly PipeType FixedVertical = new PipeType(6, "gfx/vertical.png", new List<Direction> { Direction.Up, Direction.Down }, false);
+    public static readonly PipeType FixedHorizontal = new PipeType(7, "gfx/horizontal.png", new List<Direction> { Direction.Left, Direction.Right }, false);
+    public static readonly PipeType FixedRightDown = new PipeType(8, "gfx/rightDown.png", new List<Direction> { Direction.Right, Direction.Down }, false);
+    public static readonly PipeType FixedLeftDown = new PipeType(9, "gfx/leftDown.png", new List<Direction> { Direction.Left, Direction.Down }, false);
+    public static readonly PipeType FixedRightUp = new PipeType(10, "gfx/rightUp.png", new List<Direction> { Direction.Right, Direction.Up }, false);
+    public static readonly PipeType FixedLeftUp = new PipeType(11, "gfx/leftUp.png", new List<Direction> { Direction.Left, Direction.Up }, false);
+
+    public static readonly PipeType Pump = new PipeType(12, "gfx/pump.png", new List<Direction> { Direction.Down }, false, true);
+    public static readonly PipeType Drain = new PipeType(13, "gfx/drain.png", new List<Direction> { Direction.Up, Direction.Down, Direction.Left, Direction.Right }, false, false);
+    public static readonly PipeType Obstacle = new PipeType(14, "gfx/demoblock.png", new List<Direction>(), false);
+    public static readonly PipeType LeftRightDown = new PipeType(15, "gfx/leftRightDown.png", new List<Direction> { Direction.Down, Direction.Left, Direction.Right }, false);
+    public static readonly PipeType LeftRightUp = new PipeType(16, "gfx/leftRightUp.png", new List<Direction> { Direction.Up, Direction.Left, Direction.Right }, false);
+    public static readonly PipeType LeftUpDown = new PipeType(17, "gfx/leftUpDown.png", new List<Direction> { Direction.Down, Direction.Left, Direction.Up }, false);
+    public static readonly PipeType RightUpDown = new PipeType(18, "gfx/rightUpDown.png", new List<Direction> { Direction.Down, Direction.Up, Direction.Right }, false);
+    public static readonly PipeType CrossPipe = new PipeType(19, "gfx/crossPipe.png", new List<Direction> { Direction.Down, Direction.Up, Direction.Right, Direction.Left }, false);
+
+    public static List<PipeType> Values(bool includeComplex)
+    {
+        var array = new List<PipeType>
+        {
+            Vertical,
+            Horizontal,
+            RightDown,
+            LeftDown,
+            RightUp,
+            LeftUp
+        };
+
+        if (includeComplex)
+        {
+            array.AddRange(new[] { LeftRightDown, LeftRightUp, LeftUpDown, RightUpDown });
+        }
+
+        return array;
+    }
+
+    public static List<PipeType> ComplexValues()
+    {
+        return new List<PipeType> { LeftRightDown, LeftRightUp, LeftUpDown, RightUpDown };
+    }
+
+    public static List<PipeType> Obstacles()
+    {
+        return new List<PipeType>
+        {
+            Obstacle,
+            FixedVertical,
+            FixedHorizontal,
+            FixedRightDown,
+            FixedLeftDown,
+            FixedRightUp,
+            FixedLeftUp
+        };
+    }
+}
+
+public class PipeType
+{
+    public int Value { get; }
+    public string GraphicPath { get; }
+    public List<Direction> Directions { get; }
+    public bool Replaceable { get; }
+    public bool IsPump { get; }
+
+    public PipeType(int value, string graphicPath, List<Direction> directions, bool replaceable, bool isPump = false)
+    {
+        Value = value;
+        GraphicPath = graphicPath;
+        Directions = directions;
+        Replaceable = replaceable;
+        IsPump = isPump;
+    }
+
+    //public Pipe Create()
+    //{
+    //    var graphic = LoadGraphic(GraphicPath);
+    //    var pipe = new Pipe(graphic, Directions, this, Replaceable);
+    //    if (IsPump)
+    //    {
+    //        pipe.SetAsPump();
+    //    }
+    //    return pipe;
+    //}
+
+    //private Sprite LoadGraphic(string path)
+    //{
+    //    // Load graphic from resources or asset bundle
+    //    return Resources.Load<Sprite>(path);
+    //}
+}
